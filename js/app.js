ef2ad1c693a4902a536ed1e2abba4c6cda777120
xhusd <990331630@qq.com>
2026-07-24 14:24:20 +0800

    图纸更新

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ class PixelArtGenerator {
         };
         
         // 版本号
-        this.APP_VERSION = '1.0.1';
+        this.APP_VERSION = '1.0.2';
         
         // 初始化模块
         this.pixelator = new Pixelator();
